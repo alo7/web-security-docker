3dac8dedeb1bc0faac6405b897bee733e51e4df4
BlackHole1 <158blackhole@gmail.com>
2019-10-17 09:26:36 +0800

    refactor: optimization of some small details

--- back-end/JSON Hijacking/README.md
+++ back-end/JSON Hijacking/README.md
@@ -11,6 +11,6 @@ docker-compose up --build --force-recreate
 
 使用账号 `william` 和密码 `000000` 进行登陆。
 
-登陆成功后，再打开 [http://127.0.0.1:8089](http://127.0.0.1:8089)
+登陆成功后，再打开 [http://127.0.0.1:8088](http://127.0.0.1:8088)
 
-现在去看看 `/back-end/JSON\ Hijacking/hacker_web/info.txt` 文件里的内容吧
+现在去看看 `./back-end/JSON\ Hijacking/hacker_web/info.txt` 文件里的内容吧
--- back-end/XSS/non-persistent/src/index.php
+++ back-end/XSS/non-persistent/src/index.php
@@ -1,10 +1,20 @@
 <?php
   // 关闭浏览器的XSS检测机制
   header("X-XSS-Protection: 0;");
-
-  $bg = $_GET['bg'];
-  if (empty($bg)) {
-    $bg = '999';
-  }
-  echo "<div style='width: 120px; height: 120px; background-color:#$bg'>我是一只小方块</div>";
 ?>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>XSS - 反射XSS</title>
+  </head>
+  <body>
+    <?php
+      $bg = $_GET['bg'];
+      if (empty($bg)) {
+        $bg = '999';
+      }
+      echo "<div style='width: 120px; height: 120px; background-color:#$bg'>我是一只小方块</div>";
+    ?>
+  </body>
+</html>
